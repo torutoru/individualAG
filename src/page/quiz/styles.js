d13ef915345fdac197d61b825df4f6d620aa8457
yhseo <yhseo@hancom.com>
2025-10-29 15:31:21 +0900

    반응형 위한 중앙 정렬 추가

--- src/page/quiz/styles.js
+++ src/page/quiz/styles.js
@@ -4,6 +4,7 @@ export const MainContainer = styled('main')(({ theme }) => ({
     display: 'flex', 
     flexDirection: 'column',
     maxWidth: '1200px',
+    margin: '0 auto',
     padding: '24px',
     [theme.breakpoints.up('sm')]: {
      padding: '40px',
