c1c181ee65b5d8aa3e56fdab29e7210f4817941e
yhseo <yhseo@hancom.com>
2025-10-29 15:28:03 +0900

    Typo로 최소값 적용되지 않아서 수정

--- src/page/quiz/styles.js
+++ src/page/quiz/styles.js
@@ -3,7 +3,7 @@ import {  styled } from '@mui/material';
 export const MainContainer = styled('main')(({ theme }) => ({
     display: 'flex', 
     flexDirection: 'column',
-    maxWdith: '1200px',
+    maxWidth: '1200px',
     padding: '24px',
     [theme.breakpoints.up('sm')]: {
      padding: '40px',
